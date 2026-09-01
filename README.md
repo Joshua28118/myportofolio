1c77decda5bd27d2e51efdbd2fe9b6a76be68351
Joshua28118 <joshuaimanuel309@gmail.com>
2026-09-02 06:15:36 +0700

    Initial Django project setup

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
 Name : Joshua Imanuel Setiawan
 NPM  : 2506656854
 Class: PBP D
+=======
+<div style="display: flex; display: flex-col">
+  
+
+Name : Joshua Imanuel Setiawan
+NPM  : 2506656854
+Class: PBP D
+
+</div>
+>>>>>>> edaa8b8 (Initial Django project setup)
--- myportofolio/settings.py
+++ myportofolio/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
+PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Application definition
 
@@ -77,13 +78,28 @@ WSGI_APPLICATION = 'myportofolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Database configuration
+if PRODUCTION:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+            'OPTIONS': {
+                'options': f"-c search_path={os.getenv('SCHEMA', 'public')}"
+            }
+        }
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.1/ref/settings/#auth-password-validators
